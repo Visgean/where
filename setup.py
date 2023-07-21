9e238ffc7052a3f67e9f1db9d310ee363caccdf3
martin <Visgean@gmail.com>
2023-07-21 15:03:59 +0200

    fix manifest file

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="photos_where",
-    version="1.3",
+    version="1.4",
     description="Analyze exif data",
     long_description=long_description,
     long_description_content_type="text/markdown",
